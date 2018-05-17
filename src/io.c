4936242fa81603635f8b15cff2057a1c9e1be806
Saria Hajjar <Saria@Sarias-MacBook-Pro.local>
2018-05-17 01:57:35 +0100

    Fixed warning

--- src/io.c
+++ src/io.c
@@ -94,7 +94,7 @@ int read_buffer_mud() {
 
 void read_key(void) {
   char c = 0;
-  while ((c = getc(stdin)) && c != EOF) {
+  while ((int)(c = getc(stdin)) != -1) {
     if (beginning_of_line && c == gtd.command_char) {
       int len = 0;
       char command_buffer[BUFFER_SIZE];
