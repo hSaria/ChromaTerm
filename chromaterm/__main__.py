a96ea4feee90a14ee3b1212e8c81ba7536253a13
Saria Hajjar <sariahajjar@gmail.com>
2021-10-14 14:59:29 +0100

    Minor cleanup

--- chromaterm/__main__.py
+++ chromaterm/__main__.py
@@ -56,30 +56,26 @@ def args_init(args=None):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('program',
-                        type=str,
                         metavar='program ...',
                         nargs='?',
                         help='run a program with anything after it used as '
                         'arguments')
 
     parser.add_argument('arguments',
-                        type=str,
                         nargs=argparse.REMAINDER,
-                        help=argparse.SUPPRESS,
-                        default=[])
+                        help=argparse.SUPPRESS)
 
     parser.add_argument('--config',
                         metavar='FILE',
-                        type=str,
                         help='override config file location')
 
     parser.add_argument('--reload',
                         action='store_true',
-                        help='Reload the config of all CT instances')
+                        help='reload the config of all CT instances')
 
     parser.add_argument('--rgb',
                         action='store_true',
-                        help='Use RGB colors (default: detect support, '
+                        help='use RGB colors (default: detect support, '
                         'fallback to xterm-256)')
 
     return parser.parse_args(args=args)
@@ -467,7 +463,7 @@ def main(args=None, max_wait=None, write_default=True):
     def reload_config_handler(*_):
         config_data = read_file(args.config)
 
-        if config_data is not None:
+        if config_data:
             load_rules(config, config_data, rgb=args.rgb or None)
 
     # Trigger the initial loading
