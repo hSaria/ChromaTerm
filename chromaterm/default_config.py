e9aebce1ff7ae62e98451011ef39f0ef62b0fa8b
Saria Hajjar <sariahajjar@gmail.com>
2021-12-26 02:01:07 +0000

    Add color palette to the default rules

--- chromaterm/default_config.py
+++ chromaterm/default_config.py
@@ -7,84 +7,102 @@ from chromaterm import Color, Palette, Rule
 
 PALETTE = Palette()
 
+# https://coolors.co/f67b65-00e0d1-ef2e9f-5698c8-df99f0-a35a00-057650
+PALETTE.add_color('type-1', '#f67b65')
+PALETTE.add_color('type-2', '#00e0d1')
+PALETTE.add_color('type-3', '#ef2e9f')
+PALETTE.add_color('type-4', '#5698c8')
+PALETTE.add_color('type-5', '#df99f0')
+PALETTE.add_color('type-6', '#a35a00')
+PALETTE.add_color('type-7', '#057650')
+
+# https://coolors.co/c71800-c96901-ca9102-cab902-a2bc02-79bf02-28c501
+PALETTE.add_color('status-1', '#c71800')
+PALETTE.add_color('status-2', '#c96901')
+PALETTE.add_color('status-3', '#ca9102')
+PALETTE.add_color('status-4', '#cab902')
+PALETTE.add_color('status-5', '#a2bc02')
+PALETTE.add_color('status-6', '#79bf02')
+PALETTE.add_color('status-7', '#28c501')
+
 RULE_NUMBERS = Rule(
     r'\b(?<!\.)\d+(\.\d+)?(?!\.)\b',
-    Color('f#af5f00'),
+    Color('f.type-1', palette=PALETTE),
     'Numbers',
 )
 
 RULE_IPV4 = Rule(
     r'\b(?<!\.)((25[0-5]|(2[0-4]|[0-1]?\d)?\d)\.){3}(25[0-5]|(2[0-4]|[0-1]?\d)?\d)(/\d+)?\b',
-    Color('f#00ffff'),
+    Color('f.type-2', palette=PALETTE),
     'IPv4',
     exclusive=True,
 )
 
 RULE_IPV6 = Rule(
     r'(?i)(?<![\w:])(([\da-f]{1,4}:){7}[\da-f]{1,4}|[\da-f]{1,4}:(:[\da-f]{1,4}){1,6}|([\da-f]{1,4}:){1,2}(:[\da-f]{1,4}){1,5}|([\da-f]{1,4}:){1,3}(:[\da-f]{1,4}){1,4}|([\da-f]{1,4}:){1,4}(:[\da-f]{1,4}){1,3}|([\da-f]{1,4}:){1,5}(:[\da-f]{1,4}){1,2}|([\da-f]{1,4}:){1,6}:[\da-f]{1,4}|([\da-f]{1,4}:){1,7}:|:((:[\da-f]{1,4}){1,7}|:)|::(ffff(:0{1,4})?:)?((25[0-5]|(2[0-4]|[0-1]?\d)?\d)\.){3}(25[0-5]|(2[0-4]|[0-1]?\d)?\d)|([\da-f]{1,4}:){1,4}:((25[0-5]|(2[0-4]|[0-1]?\d)?\d)\.){3}(25[0-5]|(2[0-4]|[0-1]?\d)?\d))(%[\da-z]+)?(/\d+)?(?!:?\w)',
-    Color('f#ff00ff'),
+    Color('f.type-3', palette=PALETTE),
     'IPv6 (boundaries don\'t work here as they can be in the start or end of the match, so using lookaheads and lookbehinds instead)',
     exclusive=True,
 )
 
 RULE_MAC = Rule(
     r'(?i)\b((?<!:)([\da-f]{1,2}:){5}[\da-f]{1,2}(?!:)|(?<!\.)([\da-f]{4}\.){2}[\da-f]{4}(?!\.))\b',
-    Color('f#99a8ff'),
+    Color('f.type-4', palette=PALETTE),
     'MAC addresses',
-    exclusive=True,
-)
-
-RULE_DATE = Rule(
-    r'(?i)((?<=\W)|^)((\d{2}|\d{4})\-(0?[1-9]|1[0-2])\-(3[0-1]|[1-2]\d|0?[1-9])|(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\s+((3[0-1]|[1-2]\d|0?[1-9])(\s+\d{4})?|\d{4})|((3[0-1]|[1-2]\d|0?[1-9])\s(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(\s+\d{4})?))((?=[\WT_])|$)',
-    Color('b#af5f00'),
-    'Date in YYYY-MM-DD, YY-MM-DD, MMM (DD( YYYY)?|YYYY), or DD MMM (YYYY)? formats',
-    exclusive=True,
-)
-
-RULE_TIME = Rule(
-    r'(\b|(?<=T))(?<![\.:])((2[0-3])|[0-1]\d):[0-5]\d(:[0-5]\d([\.,]\d{3,6})?)?([\-\+](\d{2}|\d{4}))?(?![\.:])\b',
-    Color('b#af5f00'),
-    'Time in hh:mm:ss.SSSSSS-ZZZZ format (sec, msec, nsec, and timezone offset optional)',
     exclusive=True,
 )
 
 RULE_SIZE = Rule(
     r'(?i)\b\d+(\.\d+)?\s?((([KMGTPEZY](i?B)?)|B)(ps)?)\b',
     {
-        0: Color('f#875fd7'),
+        0: Color('f.type-5', palette=PALETTE),
         2: Color('bold'),
     },
     'Size, like 123G 123Gb 123Gib 1.23G 123Gbps',
     exclusive=True,
 )
 
+RULE_DATE = Rule(
+    r'(?i)((?<=\W)|^)((\d{2}|\d{4})\-(0?[1-9]|1[0-2])\-(3[0-1]|[1-2]\d|0?[1-9])|(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)\s+((3[0-1]|[1-2]\d|0?[1-9])(\s+\d{4})?|\d{4})|((3[0-1]|[1-2]\d|0?[1-9])\s(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(\s+\d{4})?))((?=[\WT_])|$)',
+    Color('b.type-6', palette=PALETTE),
+    'Date in YYYY-MM-DD, YY-MM-DD, MMM (DD( YYYY)?|YYYY), or DD MMM (YYYY)? formats',
+    exclusive=True,
+)
+
+RULE_TIME = Rule(
+    r'(\b|(?<=T))(?<![\.:])((2[0-3])|[0-1]\d):[0-5]\d(:[0-5]\d([\.,]\d{3,6})?)?([\-\+](\d{2}|\d{4}))?(?![\.:])\b',
+    Color('b.type-6', palette=PALETTE),
+    'Time in hh:mm:ss.SSSSSS-ZZZZ format (sec, msec, nsec, and timezone offset optional)',
+    exclusive=True,
+)
+
 RULE_GENERIC_BAD = Rule(
     r'(?i)\b(password|abnormal(ly)?|down|los(t|s|ing)|err(ors?)?|(den(y|ies|ied)?)|reject(ing|ed)?|drop(ped|s)?|(err\-)?disabled?|(timed?\-?out)|fail(s|ed|iure)?|disconnect(ed)?|unreachable|invalid|bad|notconnect|unusable|block(ing|ed)?|blk|inaccessible|wrong|collisions?|unsynchronized|mismatch|runts|CRC)\b',
-    Color('f#ff0000'),
+    Color('f.status-1', palette=PALETTE),
     'Generics - Bad',
 )
 
 RULE_GENERIC_AMBIGIOUS_BAD = Rule(
     r"(?i)\b(no(t|pe)?|exit(ed)?|reset(t?ing)?|discard(ed|ing)?|filter(ed)?|stop(p(ed|ing))?|never|can('?t|not))\b",
-    Color('f#afaf5f'),
+    Color('f.status-3', palette=PALETTE),
     'Generics - Ambigious bad',
 )
 
 RULE_GENERIC_NOT_TOO_BAD = Rule(
     r'(?i)\b(warnings?)\b',
-    Color('f#ffff00'),
+    Color('f.status-4', palette=PALETTE),
     'Generics - Not too bad',
 )
 
 RULE_GENERIC_AMBIGIOUS_GOOD = Rule(
     r'(?i)\b(ye(s|ah?|p)?|started|running|can)\b',
-    Color('f#5f8700'),
+    Color('f.status-5', palette=PALETTE),
     'Generics - Ambigious good',
 )
 
 RULE_GENERIC_GOOD = Rule(
     r'(?i)\b(up|ok(ay)?|permit(ed|s)?|accept(s|ed)?|enabled?|online|succe((ss(ful|fully)?)|ed(ed)?)?|connect(ed)?|reachable|valid|forwarding|synchronized)\b',
-    Color('f#00ff00'),
+    Color('f.status-7', palette=PALETTE),
     'Generics - Good',
 )
 
@@ -99,8 +117,8 @@ def generate_default_rules_yaml():
     data += '\n\nrules:'
 
     for rule in [
-            RULE_NUMBERS, RULE_IPV4, RULE_IPV6, RULE_MAC, RULE_DATE, RULE_TIME,
-            RULE_SIZE, RULE_GENERIC_BAD, RULE_GENERIC_AMBIGIOUS_BAD,
+            RULE_NUMBERS, RULE_IPV4, RULE_IPV6, RULE_MAC, RULE_SIZE, RULE_DATE,
+            RULE_TIME, RULE_GENERIC_BAD, RULE_GENERIC_AMBIGIOUS_BAD,
             RULE_GENERIC_NOT_TOO_BAD, RULE_GENERIC_AMBIGIOUS_GOOD,
             RULE_GENERIC_GOOD
     ]:
