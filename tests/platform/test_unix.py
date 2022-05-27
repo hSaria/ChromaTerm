782080f329779081f7bcfba0ab39468dc4f54a51
Saria Hajjar <sariahajjar@gmail.com>
2022-05-27 17:55:59 +0100

    Fix unix test

--- tests/platform/test_unix.py
+++ tests/platform/test_unix.py
@@ -4,6 +4,7 @@ import sys
 import chromaterm.platform.unix
 
 
-def test_get_stdin():
-    '''Ensure stdin is the correct file descriptor.'''
-    assert chromaterm.platform.unix.get_stdin() == sys.stdin.fileno()
+def test_get_stdin(monkeypatch):
+    '''Ensure stdin is a file descriptor.'''
+    monkeypatch.setattr(sys.stdin, 'fileno', lambda: 0)
+    assert isinstance(chromaterm.platform.unix.get_stdin(), int)
