2e3dc6da15aec02cfddf72a20805954a648be5b8
Saria Hajjar <sariahajjar@gmail.com>
2022-06-18 16:15:04 +0100

    Post-release version bump

--- chromaterm/__init__.py
+++ chromaterm/__init__.py
@@ -3,7 +3,7 @@ import re
 import sys
 import time
 
-__version__ = '0.10.5'
+__version__ = '0.10.6-dev'
 
 # Color types, their color codes if it's style, their default reset codes, and
 # RegEx's for detecting their color type.
